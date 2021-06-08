c4b640d62f7e944d7ea1f57262e616a6336d2260
ChuaYuHan <19020844@myrp.edu.sg>
2021-06-08 12:39:58 +0800

    updated fragment_vaccination and VaccinationFragment

--- app/src/main/java/sg/edu/rp/c346/id19020125/l07_my_data_book/VaccinationFragment.java
+++ app/src/main/java/sg/edu/rp/c346/id19020125/l07_my_data_book/VaccinationFragment.java
@@ -1,5 +1,7 @@
 package sg.edu.rp.c346.id19020125.l07_my_data_book;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +9,53 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link VaccinationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class VaccinationFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public VaccinationFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment VaccinationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static VaccinationFragment newInstance(String param1, String param2) {
-        VaccinationFragment fragment = new VaccinationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    TextView tv;
+    Button btnEdit;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vaccination, container, false);
+        View view =  inflater.inflate(R.layout.fragment_vaccination, container, false);
+
+        tv = view.findViewById(R.id.textView);
+        btnEdit = view.findViewById(R.id.buttonEdit);
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(getContext());
+                alertDialog.setTitle("Edit Vaccination");
+
+                EditText etText  = new EditText(getContext());
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT);
+                etText.setLayoutParams(params);
+
+                alertDialog.setView(etText);
+
+                alertDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        tv.setText(etText.getText().toString());
+                    }
+                });
+
+                alertDialog.setNegativeButton("Cancel", null);
+
+                alertDialog.show();
+
+            }
+        });
+
+        return view;
     }
 }
