1f0fa61a74def839428c0902e1235ba469b5a5bf
19020125 <19020125@myrp.edu.sg>
2021-06-08 12:33:30 +0800

    About us

---
+++ app/src/main/java/sg/edu/rp/c346/id19020125/l07_my_data_book/AboutUs.java
@@ -0,0 +1,29 @@
+package sg.edu.rp.c346.id19020125.l07_my_data_book;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class AboutUs extends AppCompatActivity {
+
+    ImageView iv;
+    ActionBar ab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+
+        iv = findViewById(R.id.imageView);
+        String schLogo = "https://upload.wikimedia.org/wikipedia/commons/8/80/Republic_Polytechnic_Logo.jpg";
+        Picasso.with(this).load(schLogo).into(iv);
+
+        ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+}
