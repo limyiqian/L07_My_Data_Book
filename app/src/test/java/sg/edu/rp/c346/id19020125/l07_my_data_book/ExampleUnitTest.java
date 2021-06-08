08698b6cef2becaca21883783104786d61773a96
19020125 <19020125@myrp.edu.sg>
2021-06-08 10:57:33 +0800

    Initial commit

---
+++ app/src/test/java/sg/edu/rp/c346/id19020125/l07_my_data_book/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package sg.edu.rp.c346.id19020125.l07_my_data_book;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
